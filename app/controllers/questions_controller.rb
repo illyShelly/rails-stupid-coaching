76b4755be23f39b62030e48b08507765442dd1a7
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-07 14:56:24 +0100

    Stupid coaching - controller, view, routes

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,29 @@
+class QuestionsController < ApplicationController
+
+  def home
+  end
+
+  def ask
+  end
+
+  def answer
+    # apply logic if nothing in querry etc. => @answer of coach
+    @question = params[:question_me]
+    if (@question.blank?)
+      @answer = "What's the qustion?"
+    elsif @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+
+# If the message is "I am going to work", the coach will answer "Great!"
+
+# If the message has a question mark ? at the end, the coach will answer "Silly question, get dressed and go to work!".
+
+# Otherwise he will answer "I don't care, get dressed and go to work!"
